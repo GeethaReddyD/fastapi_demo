cad8af5b7707e55f9502b9faa9baa66fe86d5eae
GeethaReddyD <geethareddy.d999@gmail.com>
2022-09-14 18:51:45 +0530

    baisc setup for fastApi

---
+++ user/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI,Depends,status
+from . import schemas,models 
+from .database import engine,SessionLocal 
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(engine)
+
+app = FastAPI() 
+
+#To have independent db connection per request and then close it after request is finished
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+
+
