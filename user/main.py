47afa419db9e0718361c9971c01a81bb3f2432cd
GeethaReddyD <geethareddy.d999@gmail.com>
2022-09-14 19:05:59 +0530

    created post route

--- user/main.py
+++ user/main.py
@@ -15,6 +15,17 @@ def get_db():
         yield db
     finally:
         db.close()
+    
+# To insert new user into the table
+
+@app.post("/create",status_code = status.HTTP_201_CREATED)
+def create_user(request : schemas.User ,db : Session = Depends(get_db)):
+    new_user = models.User(name = request.name ,age = request.age ,location = request.location)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user) 
+    return new_user
+
 
 
 
