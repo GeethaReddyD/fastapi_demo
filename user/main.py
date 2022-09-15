3f5f6298f8f4ee9328b0285f3d04a83931c899a0
GeethaReddyD <geethareddy.d999@gmail.com>
2022-09-15 12:06:25 +0530

    changed model for user

--- user/main.py
+++ user/main.py
@@ -16,15 +16,7 @@ def get_db():
     finally:
         db.close()
     
-# To insert new user into the table
 
-@app.post("/create",status_code = status.HTTP_201_CREATED)
-def create_user(request : schemas.User ,db : Session = Depends(get_db)):
-    new_user = models.User(name = request.name ,email = request.email ,password = request.password)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user) 
-    return new_user
     
 
 
