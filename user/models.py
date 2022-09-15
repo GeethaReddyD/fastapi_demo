c9fb980b2fa9cddac933a57eb3c255fb5882c7d0
GeethaReddyD <geethareddy.d999@gmail.com>
2022-09-15 16:23:37 +0530

    changes in model

--- user/models.py
+++ user/models.py
@@ -3,8 +3,8 @@ from sqlalchemy import  Column, Integer, String
 from .database import Base
 
 
-class User(Base):
-    __tablename__ = "user_details"
+class Userdetail(Base):
+    __tablename__ = "userdetails"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(150) , index = True)
